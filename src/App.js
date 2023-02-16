d758f16dcc57a16db5dbe6cc4cfcff880d49c6b5
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2023-02-16 22:54:01 +0530

    small change

--- src/App.js
+++ src/App.js
@@ -172,7 +172,7 @@ function App() {
           </Routes>
           
           <footer class="py-5 bg-dark">
-            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Devan 2022</p></div>
+            <div class="container"><p class="m-0 text-center text-white">Copyright &copy; 2023</p></div>
           </footer>
 
         </body>
